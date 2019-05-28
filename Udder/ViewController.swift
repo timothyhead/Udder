9dc04de02a5b1082a0062754fc9cb493e7d62b72
timothyhead <head.timothy3@googlmail.com>
2019-05-28 15:49:05 +0100

    test2

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func test() {
+        
+    }
+    func test2(){
+        
+    }
 }
 
